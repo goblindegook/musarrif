b93cfd25873674a14b20e54411e96844b307ab75
Luis Rodrigues <lrodrigues@newstore.com>
2026-03-09 23:35:11 +0100

    refactor: form I patterns

--- src/paradigms/form-i-vowels.ts
+++ src/paradigms/form-i-vowels.ts
@@ -3,30 +3,36 @@ import type { Verb } from './verbs'
 
 export type FormIPattern =
   | 'fa3ala-yaf3alu'
-  | 'fa3ala-yaf3ulu'
   | 'fa3ala-yaf3ilu'
+  | 'fa3ala-yaf3ulu'
   | 'fa3ila-yaf3alu'
-  | 'fa3ila-yaf3ulu'
   | 'fa3ila-yaf3ilu'
+  | 'fa3ila-yaf3ulu'
+  | 'fa3ula-yaf3alu'
+  | 'fa3ula-yaf3ilu'
   | 'fa3ula-yaf3ulu'
 
 const FORM_I_PAST_VOWELS = {
   'fa3ala-yaf3alu': FATHA,
-  'fa3ala-yaf3ulu': FATHA,
   'fa3ala-yaf3ilu': FATHA,
+  'fa3ala-yaf3ulu': FATHA,
   'fa3ila-yaf3alu': KASRA,
-  'fa3ila-yaf3ulu': KASRA,
   'fa3ila-yaf3ilu': KASRA,
+  'fa3ila-yaf3ulu': KASRA,
+  'fa3ula-yaf3alu': DAMMA,
+  'fa3ula-yaf3ilu': DAMMA,
   'fa3ula-yaf3ulu': DAMMA,
 } satisfies Record<FormIPattern, Vowel>
 
 const FORM_I_PRESENT_VOWELS = {
   'fa3ala-yaf3alu': FATHA,
-  'fa3ala-yaf3ulu': DAMMA,
   'fa3ala-yaf3ilu': KASRA,
+  'fa3ala-yaf3ulu': DAMMA,
   'fa3ila-yaf3alu': FATHA,
-  'fa3ila-yaf3ulu': DAMMA,
   'fa3ila-yaf3ilu': KASRA,
+  'fa3ila-yaf3ulu': DAMMA,
+  'fa3ula-yaf3alu': FATHA,
+  'fa3ula-yaf3ilu': KASRA,
   'fa3ula-yaf3ulu': DAMMA,
 } satisfies Record<FormIPattern, Vowel>
 
