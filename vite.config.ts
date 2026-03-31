ff106d7a913ea3e81554f6840f2fb22c5b2ad7cb
Luis Rodrigues <lrodrigues@newstore.com>
2026-03-31 23:15:29 +0200

    feat: icon

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,9 @@ export default defineConfig({
       includeAssets: [
         'icon.png',
         'icon-192.png',
-        'maskable-icon.png',
-        'maskable-icon-192.png',
+        'apple-touch-icon.png',
+        'favicon-32x32.png',
+        'favicon-16x16.png',
         'fonts/NotoSansArabic-Regular.ttf',
         'fonts/NotoSansArabic-Medium.ttf',
         'fonts/NotoSansArabic-SemiBold.ttf',
@@ -34,8 +35,8 @@ export default defineConfig({
         lang: 'en',
         dir: 'ltr',
         icons: [
-          { src: 'icon.png', sizes: '512x512', type: 'image/svg+xml', purpose: 'any' },
-          { src: 'maskable-icon.svg', type: 'image/svg+xml', purpose: 'maskable' },
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
         ],
       },
     }),
