30723297e374edf6b69d365da5db89b7cbc3eaf6
Luis Rodrigues <lrodrigues@newstore.com>
2026-05-06 14:32:37 +0200

    test: TypeScript checker

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -1,13 +1,19 @@
 export default {
   packageManager: 'npm',
-  plugins: ['@stryker-mutator/vitest-runner'],
+  plugins: ['@stryker-mutator/vitest-runner', '@stryker-mutator/typescript-checker'],
   testRunner: 'vitest',
+  checkers: ['typescript'],
   reporters: ['clear-text', 'progress', 'html'],
   mutate: ['src/paradigms/**/*.{ts,tsx}', '!src/paradigms/**/*.test.{ts,tsx}'],
   testFiles: ['src/paradigms/**/*.test.{ts,tsx}'],
   cleanTempDir: 'always',
+  ignoreStatic: true,
+  incremental: true,
+  timeoutFactor: 2,
   vitest: {
     configFile: 'vitest.config.ts',
   },
-  ignoreStatic: true,
+  typescriptChecker: {
+    prioritizePerformanceOverAccuracy: true,
+  },
 }
